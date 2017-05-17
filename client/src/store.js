7ab750c815f5b6c7b21f8dc20e6cb3cb52e37ca2
cfv7 <hq@colin.red>
2017-05-16 21:46:47 -0400

    remove redux-logger

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,11 @@
 import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
-import {createLogger} from 'redux-logger'
+// import {createLogger} from 'redux-logger'
 
 import {usersReducer} from './reducers'
 
 export default createStore(
   usersReducer, 
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk, createLogger())
+  applyMiddleware(thunk) //createLogger()
 );
